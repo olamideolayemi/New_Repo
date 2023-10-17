54d649d1d30bc0f8256089811fe8b4658bcee20b
olamideolayemi <olamid15@gmail.com>
2023-10-17 12:08:52 +0100

    The first diff is adding necessary header files and return statement in add-numbers.c
    The second diff is fixing a typo in functions1.c (generated by useblackbox.ai )

--- functions1.c
+++ functions1.c
@@ -132,7 +132,7 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
-	int = BUFF_SIZE - 2;
+	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
